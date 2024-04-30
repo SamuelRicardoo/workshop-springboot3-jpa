67f806505249ab6773a85a22fafa5158daf5671b
SamuelRicardoo <sricardosgomes@hotmail.com>
2024-04-30 09:25:01 -0300

    creat class order

--- src/main/java/com/br/dev/curso/entities/User.java
+++ src/main/java/com/br/dev/curso/entities/User.java
@@ -1,21 +1,23 @@
 package com.br.dev.curso.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
-
 @Entity
-@Table(name="tb_user")
-public class User implements Serializable{
-	
+@Table(name = "tb_user")
+public class User implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -23,10 +25,12 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
-	
+
+	@OneToMany(mappedBy = "cliente")
+	private List<Order> order = new ArrayList<>();
+
 	public User() {
 	}
-
 
 	public User(Long id, String name, String email, String phone, String password) {
 		super();
@@ -73,20 +77,18 @@ public class User implements Serializable{
 		return password;
 	}
 
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
 	}
 
-
+	public List<Order> getOrder() {
+		return order;
+	}
 
 	@Override
 	public boolean equals(Object obj) {
@@ -99,13 +101,5 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
